635a13567ac2762d56274c15b902c25e8ac3c568
Aman Singh <amanrawat6767@gmail.com>
2025-01-04 23:37:45 +0530

    add users in search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -13,6 +14,7 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreen extends State<SearchScreen> {
   final TextEditingController _searchController = TextEditingController();
   bool _isShowUsers = false;
+
   @override
   void dispose() {
     super.dispose();
@@ -34,36 +36,65 @@ class _SearchScreen extends State<SearchScreen> {
           },
         ),
       ),
-      body: _isShowUsers ? FutureBuilder(
-        future: FirebaseFirestore.instance
-            .collection('users')
-            .where(
-              'username',
-              isGreaterThanOrEqualTo: _searchController.text,
-            )
-            .get(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
+      body: _isShowUsers
+          ? FutureBuilder(
+              future: FirebaseFirestore.instance
+                  .collection('users')
+                  .where(
+                    'username',
+                    isGreaterThanOrEqualTo: _searchController.text,
+                  )
+                  .get(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
 
-          return ListView.builder(
-            itemCount: (snapshot.data!.docs.length),
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                    (snapshot.data! as dynamic).docs[index]['photoUrl'],
-                  ),
-                ),
-                title: Text((snapshot.data! as dynamic).docs[index]['username'],),
-              );
-            },
-          );
-        },
-      ) : const Text('Posts'),
+                return ListView.builder(
+                  itemCount: (snapshot.data! as dynamic).docs.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          (snapshot.data! as dynamic).docs[index]['photoUrl'],
+                        ),
+                      ),
+                      title: Text(
+                        (snapshot.data! as dynamic).docs[index]['username'],
+                      ),
+                    );
+                  },
+                );
+              },
+            )
+          : FutureBuilder(
+              future: FirebaseFirestore.instance.collection('posts').get(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                return MasonryGridView.count(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                  itemCount: (snapshot.data! as dynamic).docs.length,
+                  itemBuilder: (context, index) {
+                    double aspectRatio = (index % 7 == 0) ? 1.0 : 0.5;
+                    return AspectRatio(
+                      aspectRatio: aspectRatio,
+                      child: Image.network(
+                        fit: BoxFit.cover,
+                          (snapshot.data! as dynamic).docs[index]['postUrl']),
+                    );
+                  },
+                );
+              },
+            ),
     );
   }
 }
